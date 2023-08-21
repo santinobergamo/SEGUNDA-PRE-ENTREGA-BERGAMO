938b4f0841e17ca79517b41a9b1496cc1af5e68f
Santino Bergamo <santinobergamo@gmail.com>
2023-08-21 17:56:26 -0300

    version final de mi codigo

---
+++ build/lib/Pagina_de_compras/Modulo2.py
@@ -0,0 +1,21 @@
+from Paquete1.Modulo1 import *
+class Comprar():
+    def __init__(self,compra):
+        self.compra = compra
+
+def main():
+    clientes = []
+    while True:
+        cliente = Cliente()
+        compra = input("Que llevo? ")
+        compra_cliente = Comprar(compra)
+        cliente.compra = compra_cliente.compra
+        clientes.append(cliente)
+
+        continuar = input("¿Desea ingresar información para otro cliente? (s/n): ")
+        if continuar.lower() != 's':
+            break
+    
+    for cliente in clientes:
+        print("------------------------------------------------")
+        print(cliente)
